bbceb6f7350e574cc07fb9cd592594e2cc361440
Nazarii Kolesnikov <nazarkolesnikov148@ukr.net>
2026-06-27 04:24:38 +0300

    next commit

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 import Image from 'next/image';
 import MugunghwaBloomsRootsLogo from "../../assets/logo/MugunghwaBloomsRootsLogo/transparent/MugunghwaBloomsRootsTransparentLogo.png";
@@ -18,70 +21,139 @@ import PinterestLogo from '../../assets/logo/PinterestLogo/PinterestLogo';
 import PhoneLogo from '../../assets/logo/PhoneLogo/PhoneLogo';
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(() => {
+        if (isMenuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+    }, [isMenuOpen]);
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
     return (
         <>
-            <div className={styles.salesAndPropositions}>
-                <div className={styles.salesAndPropositionsWrapper}>
-                    <a href="email:mugunghwarootandbloomsteam@gmail.com" className={styles.phoneButton}>
-                        <PhoneLogo className={styles.phoneButtonLogo} />
-                        <span className={styles.phoneButtonText}>mugunghwarootandbloomsteam@gmail.com</span>
-                    </a>
-                    <a href="tel:+380678333023" className={styles.phoneButton}>
-                        <PhoneLogo className={styles.phoneButtonLogo} />
-                        <span className={styles.phoneButtonText}>(067)-833-30-23</span>
-                    </a>
-                    <InstagramLogo className={`${styles.socialIconLogo} ${styles.instagramLogo}`} />
-                    <FacebookLogo className={`${styles.socialIconLogo} ${styles.facebookLogo}`} />
-                    <TikTokLogo className={`${styles.socialIconLogo} ${styles.tikTokLogo}`} />
-                    <TelegramLogo className={`${styles.socialIconLogo} ${styles.telegramLogo}`} />
-                    <ViberLogo className={`${styles.socialIconLogo} ${styles.viberLogo}`} />
-                    <LinkedInLogo className={`${styles.socialIconLogo} ${styles.linkedInLogo}`} />
-                    <XLogo className={`${styles.socialIconLogo} ${styles.xLogo}`} />
-                    <PinterestLogo className={`${styles.socialIconLogo} ${styles.pinterestLogo}`} />
+            <div className={styles.desktopContainer}>
+                <div className={styles.salesAndPropositions}>
+                    <div className={styles.salesAndPropositionsWrapper}>
+                        <a href="email:mugunghwarootandbloomsteam@gmail.com" className={styles.phoneButton}>
+                            <PhoneLogo className={styles.phoneButtonLogo} />
+                            <span className={styles.phoneButtonText}>mugunghwarootandbloomsteam@gmail.com</span>
+                        </a>
+                        <a href="tel:+380678333023" className={styles.phoneButton}>
+                            <PhoneLogo className={styles.phoneButtonLogo} />
+                            <span className={styles.phoneButtonText}>(067)-833-30-23</span>
+                        </a>
+                        <InstagramLogo className={`${styles.socialIconLogo} ${styles.instagramLogo}`} />
+                        <FacebookLogo className={`${styles.socialIconLogo} ${styles.facebookLogo}`} />
+                        <TikTokLogo className={`${styles.socialIconLogo} ${styles.tikTokLogo}`} />
+                        <TelegramLogo className={`${styles.socialIconLogo} ${styles.telegramLogo}`} />
+                        <ViberLogo className={`${styles.socialIconLogo} ${styles.viberLogo}`} />
+                        <LinkedInLogo className={`${styles.socialIconLogo} ${styles.linkedInLogo}`} />
+                        <XLogo className={`${styles.socialIconLogo} ${styles.xLogo}`} />
+                        <PinterestLogo className={`${styles.socialIconLogo} ${styles.pinterestLogo}`} />
+                    </div>
                 </div>
+                <header className={styles.header}>
+                    <div className={styles.logo}>
+                        <Image src={MugunghwaBloomsRootsLogo} className={styles.mugunghwaBloomsRootsLogo} alt="Mugunghwa Blooms Roots Logo" />
+                    </div>
+                    <div className={styles.searchContainer}>
+                        <input 
+                            type="text" 
+                            placeholder="Будь ласка, введіть назву товару, категорію або бренд ..."
+                            className={styles.searchInput} 
+                        />
+                        <button className={styles.searchBtn}>
+                            <Image className={styles.searchBtnImg} src="https://i.ibb.co/WWWYd5jC/1150612.png" alt="Search Button Icon" width={20} height={20} style={{ paddingRight: "4px" }}/>
+                            Шукати
+                        </button>
+                    </div>
+                    <div className={styles.userActions}>
+                        <div className={styles.actionItem}>
+                            <CatalogLogo className={styles.catalogLogo} /> Каталог
+                        </div>
+                        <div className={styles.actionItem}>
+                            <NewProductsLogo className={styles.catalogLogo} /> Акції та новинки
+                        </div>
+                        <div className={styles.actionItem}>
+                            <SavesLogo className={styles.catalogLogo} /> Закладки
+                        </div>
+                        <div className={styles.actionItem}>
+                            <FavoritesLogo className={styles.catalogLogo} /> Улюблене
+                        </div>
+                        <div className={styles.actionItem}>
+                            <ShoppingCartLogo className={styles.catalogLogo} /> Кошик
+                        </div>
+                        <div className={styles.actionItem}>
+                            <AuthorizationLogo className={styles.catalogLogo} /> Увійти
+                        </div>
+                    </div>
+                </header>
             </div>
-            <header className={styles.header}>
-                <div className={styles.logo}>
-                    <Image src={MugunghwaBloomsRootsLogo} className={styles.mugunghwaBloomsRootsLogo} alt="Mugunghwa Blooms Roots Logo" />
+            <div className={styles.mobileContainer}>
+                <div 
+                    className={`${styles.overlay} ${isMenuOpen ? styles.overlayActive : ''}`} 
+                    onClick={toggleMenu}
+                >
                 </div>
-                <div className={styles.searchContainer}>
-                    <input 
-                        type="text" 
-                        placeholder="Будь ласка, введіть назву товару, категорію або бренд ..."
-                        className={styles.searchInput} 
-                    />
-                    <button className={styles.searchBtn}>
-                        <Image className={styles.searchBtnImg} src="https://i.ibb.co/WWWYd5jC/1150612.png" alt="Search Button Icon" width={20} height={20} style={{ paddingRight: "4px" }}/>
-                        Шукати
-                    </button>
-                </div>
-                <div className={styles.userActions}>
-                    <div className={styles.actionItem}>
-                        <CatalogLogo className={styles.catalogLogo} />
-                        Каталог
+                <div className={`${styles.mobileSidebar} ${isMenuOpen ? styles.sidebarOpen : ''}`}>
+                    <div className={styles.sidebarHeader}>
+                        <span className={styles.sidebarTitle}>Меню</span>
+                        <button className={styles.closeBtn} onClick={toggleMenu}>&times;</button>
                     </div>
-                    <div className={styles.actionItem}>
-                        <NewProductsLogo className={styles.catalogLogo} />
-                        Акції та новинки
-                    </div>
-                     <div className={styles.actionItem}>
-                        <SavesLogo className={styles.catalogLogo} />
-                        Закладки
-                    </div>
-                    <div className={styles.actionItem}>
-                        <FavoritesLogo className={styles.catalogLogo} />
-                        Улюблене
-                    </div>
-                    <div className={styles.actionItem}>
-                        <ShoppingCartLogo className={styles.catalogLogo} />
-                        Кошик
-                    </div>
-                    <div className={styles.actionItem}>
-                        <AuthorizationLogo className={styles.catalogLogo} />
-                        Увійти
+                    <div className={styles.sidebarContent}>
+                        <div className={styles.sidebarActions}>
+                            <div className={styles.sidebarActionItem}><CatalogLogo className={styles.catalogLogoMobile} /> Каталог</div>
+                            <div className={styles.sidebarActionItem}><NewProductsLogo className={styles.catalogLogoMobile} /> Акції та новинки</div>
+                            <div className={styles.sidebarActionItem}><SavesLogo className={styles.catalogLogoMobile} /> Закладки</div>
+                            <div className={`${styles.sidebarActionItem} ${styles.favoritesGoodsActionItem}`}><FavoritesLogo className={styles.catalogLogoMobile} /> Улюблене</div>
+                            <div className={`${styles.sidebarActionItem} ${styles.authorizeToSystemActionItem}`}><AuthorizationLogo className={styles.catalogLogoMobile} /> Увійти</div>
+                        </div>
+                        <div className={styles.sidebarDivider}></div>
+                        <div className={styles.sidebarActions}>
+                            <InstagramLogo className={`${styles.sidebarActionItem} ${styles.socialIconLogo} ${styles.instagramLogo}`} />
+                            <FacebookLogo className={`${styles.sidebarActionItem}  ${styles.socialIconLogo} ${styles.facebookLogo}`} />
+                            <TikTokLogo className={`${styles.socialIconLogo} ${styles.tikTokLogo}`} />
+                            <TelegramLogo className={`${styles.socialIconLogo} ${styles.telegramLogo}`} />
+                            <ViberLogo className={`${styles.socialIconLogo} ${styles.viberLogo}`} />
+                            <LinkedInLogo className={`${styles.socialIconLogo} ${styles.linkedInLogo}`} />
+                            <XLogo className={`${styles.socialIconLogo} ${styles.xLogo}`} />
+                            <PinterestLogo className={`${styles.socialIconLogo} ${styles.pinterestLogo}`} />
+                        </div>
+                        <div className={styles.sidebarContacts}>
+                            <a href="tel:+380678333023" className={styles.phoneButtonMobile}>
+                                <PhoneLogo className={styles.phoneButtonLogoMobile} />
+                                <span className={styles.phoneButtonTextMobile}>(067)-833-30-23</span>
+                            </a>
+                        </div> 
                     </div>
                 </div>
-            </header>
+                <header className={styles.mobileHeader}>
+                    <div className={styles.mobileHeaderTop}>
+                        <button className={styles.burgerBtn} onClick={toggleMenu}>
+                            <span className={styles.burgerLine}></span>
+                            <span className={styles.burgerLine}></span>
+                            <span className={styles.burgerLine}></span>
+                        </button>
+                        <div className={styles.logoMobile}>
+                            <Image src={MugunghwaBloomsRootsLogo} className={styles.mugunghwaBloomsRootsLogoMobile} alt="Mugunghwa Blooms Roots Logo" />
+                        </div>
+                        <div className={styles.mobileCartBtn}>
+                            <ShoppingCartLogo className={styles.cartLogoMobile} />
+                        </div>
+                    </div>
+                    <div className={styles.mobileSearchContainer}>
+                        <input 
+                            type="text" 
+                            placeholder="Пошук товарів..."
+                            className={styles.mobileSearchInput} 
+                        />
+                    </div>
+                </header>
+            </div>
         </>  
     );
 }
